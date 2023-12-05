6105ebf257adba37d4756e5b77566f8879ff8b98
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2023-12-06 04:41:22 +0500

    feat: custom fire notification

--- src/components/Fire.jsx
+++ src/components/Fire.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { useFirebaseValue } from '../useFirebase.js';
+import addNotification from "react-push-notification";
 
 const Fire = () => {
   const isFire = useFirebaseValue('isFire');
@@ -8,18 +9,43 @@ const Fire = () => {
   const requestAndSendNotification = () => {
     if (!window.Notification) return;
 
-    isFire && new Notification('Fire Alert', {
-      body: 'Fire is detected!!!',
-      tag: 'fire',
-    });
+    if(fire === 1) {
+      new Notification('Fire Alert', {
+        body: 'Fire is detected!!!',
+        tag: 'fire',
+      });
+
+      addNotification({
+        title: "Fire Alert",
+        subtitle: "Fire is detected!!!",
+        message: "There is a fire in the office",
+        theme: "red",
+        closeButton: "X",
+        duration: 10000,
+        backgroundTop: "darkred",
+        backgroundBottom: "maroon",
+      })
+    }
 
     if (Notification.permission !== 'granted') {
     Notification.requestPermission().then(function (permission) {
       if (permission === 'granted' && isFire) {
-        isFire && new Notification('Fire Alert', {
-          body: 'Fire is detected!!!',
-          tag: 'fire',
-        });
+        if(fire === 1) {
+          new Notification('Fire Alert', {
+            body: 'Fire is detected!!!',
+            tag: 'fire',
+          });
+
+          addNotification({
+            title: "Success",
+            subtitle: "You have successfully submitted",
+            message: "Welcome to GeeksforGeeks",
+            theme: "light",
+            closeButton: "X",
+            backgroundTop: "green",
+            backgroundBottom: "yellowgreen",
+          })
+        }
       }
     });
     }
